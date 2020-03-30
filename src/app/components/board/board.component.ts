f3d25bdd414788147dd2c2e350f0e0d791a71739
Matt Taylor <taylor.matt777@gmail.com>
2020-03-30 13:57:19 -0400

    Remove h1

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import { Game } from 'types';
+import {Game} from 'types';
 
 @Component({
   selector: 'str-board',
@@ -7,8 +7,7 @@ import { Game } from 'types';
   styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent {
-  @Input()
-  game: Game;
+  @Input() game: Game;
 
   constructor() {}
 
@@ -16,16 +15,22 @@ export class BoardComponent {
   getColor(row: number, col: number): string {
     const board = this.game.board;
     if (board) {
-      if (board.offLimits.some( offLimitCell => (offLimitCell.row === row  && offLimitCell.col === col))) {
+      if (board.offLimits.some(
+              offLimitCell =>
+                  (offLimitCell.row === row && offLimitCell.col === col))) {
         console.log('Found off limits cell at ' + row + ', ' + col);
-        return '#808080'; // Greyed out
+        return '#808080';  // Greyed out
         // color = '#808080'; // Greyed out
       }
 
       let playerColor = '';
-      board.players.forEach( player => {
-        if (player.coordinates.some(playerCell => (playerCell.row === row  && playerCell.col === col))) {
-          console.log('Found player color: ' + player.color + ' at cell ' + row + ', ' + col);
+      board.players.forEach(player => {
+        if (player.coordinates.some(
+                playerCell =>
+                    (playerCell.row === row && playerCell.col === col))) {
+          console.log(
+              'Found player color: ' + player.color + ' at cell ' + row + ', ' +
+              col);
           playerColor = player.color;
           return;
         }
@@ -39,4 +44,3 @@ export class BoardComponent {
     }
   }
 }
-
